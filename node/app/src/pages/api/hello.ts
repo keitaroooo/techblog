3f4c81f48b2b49d05ee3ceac63398f8ac0e17a40
Keitaroooo <keitaro.cryptocurrency@gmail.com>
2021-02-21 17:11:09 +0900

    Initial commit

---
+++ node/app/src/pages/api/hello.ts
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+type Response = {
+  statusCode: number
+  json({ name: string }): void
+}
+
+export default (_req: void, res: Response): void => {
+  res.status(200).json({ name: 'John Doe' })
+}
